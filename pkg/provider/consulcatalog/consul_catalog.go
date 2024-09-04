2e16f75af120dceb479d75fda2690f60cf43684a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-09-04 09:26:48 -0400

    spelling: case-insensitive
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/provider/consulcatalog/consul_catalog.go
+++ pkg/provider/consulcatalog/consul_catalog.go
@@ -581,7 +581,7 @@ func (p *Provider) watchConnectTLS(ctx context.Context) error {
 	}
 }
 
-// includesHealthStatus returns true if the status passed in exists in the configured StrictChecks configuration. Statuses are case insensitive.
+// includesHealthStatus returns true if the status passed in exists in the configured StrictChecks configuration. Statuses are case-insensitive.
 func (p *Provider) includesHealthStatus(status string) bool {
 	for _, s := range p.StrictChecks {
 		// If the "any" status is included, assume all health checks are included
